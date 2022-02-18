32ddf4c89357fa84775bc8822564cdcc3047756f
lucaslelann <lucas.lelann@gmail.com>
2022-02-18 14:45:24 +0000

    ci(tests): setup the tests folder

--- workspace.R
+++ workspace.R
@@ -1,8 +1,8 @@
 library(tercen)
 library(dplyr)
 
-options("tercen.workflowId" = "wwww")
-options("tercen.stepId"     = "dddd")
+options("tercen.workflowId" = "afd1de9b7dc4907294ea74bfec00293b")
+options("tercen.stepId"     = "3f7c3c5e-a73b-49b0-88e5-3acb60b0ed80")
 
 ctx <- tercenCtx()
 
